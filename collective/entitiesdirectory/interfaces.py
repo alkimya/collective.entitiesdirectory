e7ecc2a174651442a66d3d1376fd4918d2b73122
loc <loc@alhucema.home.org>
2009-09-10 20:41:01 +0200

    No changes

--- collective/entitiesdirectory/interfaces.py
+++ collective/entitiesdirectory/interfaces.py
@@ -1,8 +1,6 @@
 from zope.interface import Interface
 from zope.schema import Iterable
 
-from collective.entitiesdirectory.i18n import _
-
 class IEntity(Interface):
     """An entry in a entities directory. Can be a NGO, a students' group or whatever
     """
